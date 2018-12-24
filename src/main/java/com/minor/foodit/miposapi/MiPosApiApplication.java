008e4e9e69e0c3d63f3649f092cccf5a384ff57f
foodit <minorfoodit@gmail.com>
2018-12-24 14:03:10 +0700

    Initial spring boot project on local repository

---
+++ src/main/java/com/minor/foodit/miposapi/MiPosApiApplication.java
@@ -0,0 +1,14 @@
+package com.minor.foodit.miposapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPosApiApplication.class, args);
+	}
+
+}
+
